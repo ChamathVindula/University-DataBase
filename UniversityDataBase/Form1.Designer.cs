961f73b01c3fa6d7997b64ea756dda4a4c11ddee
Chamath Vindula <42168073+ChamathVindula@users.noreply.github.com>
2019-07-02 19:02:35 +1000

    Add files via upload

---
+++ UniversityDataBase/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace UniversityDataBase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Go_Button = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(42, 474);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1116, 353);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Go_Button
+            // 
+            this.Go_Button.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Go_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Go_Button.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Go_Button.Location = new System.Drawing.Point(556, 416);
+            this.Go_Button.Name = "Go_Button";
+            this.Go_Button.Size = new System.Drawing.Size(108, 43);
+            this.Go_Button.TabIndex = 1;
+            this.Go_Button.Text = "Go";
+            this.Go_Button.UseVisualStyleBackColor = false;
+            this.Go_Button.Click += new System.EventHandler(this.Go_Button_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.textBox1.Location = new System.Drawing.Point(42, 212);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1116, 198);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 28);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter Query Here";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(42, 67);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 21);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "ADD";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(42, 94);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(83, 21);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "DELETE";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(42, 121);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(60, 21);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "EDIT";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(42, 148);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(97, 21);
+            this.radioButton4.TabIndex = 7;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "RETRIEVE";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(207, 28);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Select Operation";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1199, 903);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Go_Button);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Go_Button;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ UniversityDataBase/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace UniversityDataBase
+{
+    public partial class Form1 : Form
+    {
+        private String connectionString;
+        private SqlConnection newConnection;
+        private String query;
+       
+        public Form1()
+        {
+            InitializeComponent();
+            connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\University.mdf;Integrated Security=True";
+        }
+
+        private void Go_Button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                query = textBox1.Text;
+                SelectedOption();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void SelectedOption()
+        {
+            if (radioButton1.Checked)
+            {
+                Add();
+            }
+            else if (radioButton2.Checked)
+            {
+                Delete();
+            }
+            else if (radioButton3.Checked)
+            {
+                Edit();
+            }
+            else if (radioButton4.Checked)
+            {
+                dataGridView1.DataSource = Select();
+            }
+        }
+
+        private void Add()
+        {
+            try
+            {
+                using (newConnection = new SqlConnection(connectionString))
+                {
+                    newConnection.Open();
+                    SqlCommand newCommand = new SqlCommand(query, newConnection);
+                    newCommand.ExecuteNonQuery();
+                    newConnection.Close();
+                    MessageBox.Show("Record Added!");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Delete()
+        {
+
+            try
+            {
+                using (newConnection = new SqlConnection(connectionString))
+                {
+                    newConnection.Open();
+                    SqlCommand newCommand = new SqlCommand(query, newConnection);
+                    newCommand.ExecuteNonQuery();
+                    newConnection.Close();
+                    MessageBox.Show("Record Deleted!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Edit()
+        {
+
+            try
+            {
+                using (newConnection = new SqlConnection(connectionString))
+                {
+                    newConnection.Open();
+                    SqlCommand newCommand = new SqlCommand(query, newConnection);
+                    newCommand.ExecuteNonQuery();
+                    newConnection.Close();
+                    MessageBox.Show("Record Updated!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private DataTable Select()
+        {
+
+            try
+            {
+                DataTable newTable = null;
+                using (newConnection = new SqlConnection(connectionString))
+                {
+                    newConnection.Open();
+                    SqlCommand newCommand = new SqlCommand(query, newConnection);
+                    SqlDataAdapter newAdapter = new SqlDataAdapter(newCommand);
+                    newTable = new DataTable();
+                    newAdapter.Fill(newTable);
+                    newConnection.Close();
+                }
+                return newTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+    }
+}
